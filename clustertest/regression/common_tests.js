ea09ca4731659e861045ff1f41fe6d2903e4669d
Steve Singer <ssinger@ca.afilias.info>
2011-06-08 13:10:50 -0400

    Have the regression tests specify the port

--- clustertest/regression/common_tests.js
+++ clustertest/regression/common_tests.js
@@ -136,8 +136,8 @@ function get_slonik_preamble() {
 	
 	var slonikPre = 'cluster name=slonyregress;\n';
 	for(var idx = 1; idx <= NUM_NODES; idx++) {
-		slonikPre+=  'node '+idx+' admin conninfo=\'dbname=$database.db'+idx+'.dbname host=$database.db'+idx+'.host user=$database.db'+idx+'.user.slony password=$database.db'+idx+'.password\';\n';
-		slonikPre+= 'define CONNINFO'+idx+' \'dbname=$database.db'+idx+'.dbname host=$database.db'+idx+'.host user=$database.db'+idx+'.user.slony password=$database.db'+idx+'.password\';\n';
+		slonikPre+=  'node '+idx+' admin conninfo=\'dbname=$database.db'+idx+'.dbname host=$database.db'+idx+'.host port=$database.db'+idx+'.port user=$database.db'+idx+'.user.slony password=$database.db'+idx+'.password\';\n';
+		slonikPre+= 'define CONNINFO'+idx+' \'dbname=$database.db'+idx+'.dbname host=$database.db'+idx+'.host port=$database.db'+idx+'.port user=$database.db'+idx+'.user.slony password=$database.db'+idx+'.password\';\n';
 	}
 	return slonikPre;
 
